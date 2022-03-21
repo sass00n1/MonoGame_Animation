45d508d644197bd335e09d06d0674bac46afaf7e
sass00n1 <41114110+sass00n1@users.noreply.github.com>
2022-03-21 11:34:35 +0800

    Add files via upload

---
+++ Animation.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace StartFramework
+{
+    /// <summary>
+    /// 一个动画片段的数据
+    /// </summary>
+    public class Animation
+    {
+        //纹理
+        public Texture2D Texture
+        {
+            get { return texture; }
+        }
+        Texture2D texture;
+
+        //每帧动画的持续时间
+        public float FrameTime
+        {
+            get { return frameTime; }
+        }
+        float frameTime;
+
+        //循环
+        public bool IsLooping
+        {
+            get { return isLooping; }
+        }
+        bool isLooping;
+
+        //动画有几帧
+        public int FrameCount
+        {
+            get { return Texture.Width / FrameHeight; }
+        }
+
+        //每帧的宽度
+        public int FrameWidth
+        {
+            get { return Texture.Height; }
+        }
+
+        //每帧的高度
+        public int FrameHeight
+        {
+            get { return Texture.Height; }
+        }
+
+        //构造
+        public Animation(Texture2D texture, float frameTime, bool isLooping)
+        {
+            this.texture = texture;
+            this.frameTime = frameTime;
+            this.isLooping = isLooping;
+        }
+    }
+}
---
+++ AnimationPlayer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace StartFramework
+{
+    /// <summary>
+    /// 动画播放器
+    /// </summary>
+    public struct AnimationPlayer
+    {
+        //动画
+        public Animation Animation
+        {
+            get { return animation; }
+        }
+        Animation animation;
+
+        /// <summary>
+        /// 当前的动画帧索引
+        /// </summary>
+        public int FrameIndex
+        {
+            get { return frameIndex; }
+        }
+        int frameIndex;
+
+        /// <summary>
+        /// 当前帧的时间
+        /// </summary>
+        private float time;
+
+        /// <summary>
+        /// 动画的原点，也就是精灵的脚下
+        /// </summary>
+        public Vector2 Origin
+        {
+            get { return new Vector2(Animation.FrameWidth / 2.0f, Animation.FrameHeight); }
+        }
+
+        /// <summary>
+        /// 播放
+        /// </summary>
+        public void PlayAnimation(Animation animation)
+        {
+            if (Animation == animation)
+                return;
+
+            this.animation = animation;
+            this.frameIndex = 0;
+            this.time = 0.0f;
+        }
+
+        /// <summary>
+        /// 绘制动画
+        /// </summary>
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch, Vector2 position, SpriteEffects spriteEffects)
+        {
+            if (Animation == null)
+                throw new NotSupportedException("No animation");
+
+            time += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            while (time > Animation.FrameTime)
+            {
+                time -= Animation.FrameTime;
+
+                if (Animation.IsLooping)
+                {
+                    frameIndex = (frameIndex + 1) % Animation.FrameCount;
+                }
+                else
+                {
+                    frameIndex = Math.Min(frameIndex + 1, Animation.FrameCount - 1);
+                }
+            }
+
+            Rectangle source = new Rectangle(FrameIndex * Animation.Texture.Height, 0, Animation.Texture.Height, Animation.Texture.Height);
+
+            spriteBatch.Draw(Animation.Texture, position, source, Color.White, 0.0f, Origin, 1.0f, spriteEffects, 0.0f);
+        }
+    }
+}
